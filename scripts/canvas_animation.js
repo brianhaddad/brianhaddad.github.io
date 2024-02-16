9eb81012ad08561902215bd626508bedae68cb63
Brian <brianhaddad@gmail.com>
2024-02-15 22:53:05 -0700

    Added launch trails for the fireworks and tweaked a bunch of timing. Still not super happy with it but oh well.

--- scripts/canvas_animation.js
+++ scripts/canvas_animation.js
@@ -10,14 +10,24 @@ function DrawBoundsRect(xLoc, yLoc, width, height) {
 //politely borrowed from:
 //https://www.30secondsofcode.org/js/s/hsb-to-rgb/
 //The range of the input parameters is H: [0, 360], S: [0, 100], B: [0, 100].
-function HSBToRGBstring(h, s, b) {
+function HSBToRGBstring(h, s, b, t) {
+    t ??= 1;
     const fix = (n) => Math.max(Math.min(Math.round(n), 255), 0);
     s /= 100;
     b /= 100;
     const k = (n) => (n + h / 60) % 6;
     const f = (n) => b * (1 - s * Math.max(0, Math.min(k(n), 4 - k(n), 1)));
+    if (t > 0 && t < 1) {
+        return `rgba(${fix(255 * f(5))},${fix(255 * f(3))},${fix(255 * f(1))},${t.toFixed(3)})`;
+    }
     return `rgb(${fix(255 * f(5))},${fix(255 * f(3))},${fix(255 * f(1))})`;
 }
+
+const easing = {
+    in: (t) => t * t,
+    out: (t) => 1 - (1 - t) * (1 - t),
+    inAndOut: (t) => t * t * (3.0 - 2.0 * t),
+};
 
 function LayeredAnimationCollection() {
     const layers = [];
@@ -83,7 +93,8 @@ function SequentialAnimationCollection() {
 //TODO: instead of update functions and animate functions, might need classes to handle them
 //that way we can get rid of the total animation time in the AnimationContainer class
 //and the animator can handle things like physics.
-function CountdownAnimationContainer(totalTime, updateFunc, animateFunc) {
+function CountdownAnimationContainer(totalTime, updateFunc, animateFunc, delayStart) {
+    let delay = delayStart ?? 0;
     const totalAnimationTime = totalTime;
     let timeRemaining = totalTime;
     const updater = updateFunc;
@@ -93,14 +104,83 @@ function CountdownAnimationContainer(totalTime, updateFunc, animateFunc) {
     const isActive = () => t >= 0 && t <= 1;
 
     this.update = (dt) => {
-        timeRemaining -= dt;
-        t = updater(timeRemaining, totalAnimationTime);
+        if (delay > 0) {
+            delay -= dt;
+        }
+        if (delay <= 0) {
+            timeRemaining -= dt;
+            t = updater(timeRemaining, totalAnimationTime);
+        }
         return isActive();
     };
 
     this.draw = (ctx, drawRect) => {
         if (isActive()) {
             return animator(ctx, drawRect, t);
+        }
+        return null;
+    };
+}
+
+function MakeFireworkSequence(x, y, delayStart) {
+    const sequence = new SequentialAnimationCollection();
+    sequence.addSequence(new FireworkLaunchTrail(x, y, delayStart));
+    sequence.addSequence(new FireworkExplosion(x, y));
+    return sequence;
+}
+
+function FireworkLaunchTrail(cx, cy, delayStart) {
+    let elapsedTime = 0;
+    //TODO: pass this in as a parameter? Wider launch time window?
+    const totalLaunchTime = Math.floor(Math.random() * 1250) + 750;
+    const xLocStart = cx;
+    const yLocStart = 1.1; //TODO: pass in the value?
+    const xLocTarget = cx;
+    const yLocTarget = cy;
+    let xLocCurrent = xLocStart;
+    let yLocCurrent = yLocStart;
+    let timeUntilBegin = delayStart ?? 0;
+    let active = true;
+    const particles = [];
+
+    const addTrailParticle = () => {
+        particles.push(new FireworkLaunchTrailParticle(Math.floor(Math.random() * 1000) + 500, xLocCurrent, yLocCurrent, ((Math.random() * 3) + 1) / 100));
+    };
+
+    this.update = (dt) => {
+        if (timeUntilBegin > 0) {
+            timeUntilBegin -= dt;
+        }
+
+        if (timeUntilBegin <= 0 && active) {
+            elapsedTime += dt;
+            const t = elapsedTime / totalLaunchTime;
+            xLocCurrent = xLocStart + ((easing.out(t)) * (xLocTarget - xLocStart));
+            yLocCurrent = yLocStart + ((easing.out(t)) * (yLocTarget - yLocStart));
+            let weDead = (t >= 1);
+            if (!weDead) {
+                addTrailParticle();
+            }
+            for (const particle of particles) {
+                if (particle.update(dt)) {
+                    weDead = false;
+                }
+            }
+            if (weDead) {
+                active = false;
+            }
+        }
+        return active;
+    };
+
+    this.draw = (ctx, drawRect) => {
+        if (timeUntilBegin <= 0 && active) {
+            ctx.save();
+            ctx.translate(drawRect.cx, drawRect.cy);
+            for (const particle of particles) {
+                particle.draw(ctx, drawRect);
+            }
+            ctx.restore();
         }
         return null;
     };
@@ -124,7 +204,7 @@ function FireworkExplosion(cx, cy, delayStart) {
     const getColor =() => HSBToRGBstring(getHue(), getSaturation(), getValue());
     
     const angleRange = Math.PI;
-    const sparkleAfter = Math.random() * 2 > 1 ? (Math.random() * .75) + .25 : 1;
+    const sparkleAfter = Math.random() * 2 > 1 ? (Math.random() * .5) + .5 : 1;
 
     const timeToLiveVariation = (Math.random() * .5) + .5;
     const maxTimeToLive = Math.floor(Math.random() * 5000) + 2000;
@@ -150,7 +230,6 @@ function FireworkExplosion(cx, cy, delayStart) {
         }
 
         if (timeUntilBegin <= 0 && active) {
-            //TODO: I have no idea what I'm doing! lol Apply "physics" to the particles using dt
             let weDead = true;
             for (const particle of particles) {
                 if (particle.update(dt)) {
@@ -243,6 +322,45 @@ function FireworkParticle(timeToLive, size, startVX, startVY, accX, accY, grav, 
                 ctx.fillStyle = c;
                 ctx.fill();
             }
+            ctx.restore();
+        }
+        return null;
+    };
+}
+
+function FireworkLaunchTrailParticle(timeToLive, locX, locY, maxSize) {
+    const totalLife = timeToLive;
+    let lifeRemaining = timeToLive;
+    let t = 0;
+    
+    const x = locX;
+    const y = locY;
+    const targetSize = maxSize;
+
+    const s = () => targetSize * (1 - t);
+    //TODO: the base color could be randomized/passed-in maybe? I dunno...
+    const c = () => `rgba(100,100,100,${t.toFixed(3)})`;
+
+    const isActive = () => t >= 0 && t <= 1;
+
+    this.update = (dt) => {
+        lifeRemaining -= dt;
+        t = lifeRemaining / totalLife;
+        return isActive();
+    };
+
+    this.draw = (ctx, drawRect) => {
+        if (isActive()) {
+            const baseline = Math.min(drawRect.w, drawRect.h);
+            const r = .5 * s() * baseline;
+            const px = x * baseline;
+            const py = y * baseline;
+
+            ctx.save();
+            ctx.beginPath();
+            ctx.arc(px, py, r, 0, Math.PI * 2, false);
+            ctx.fillStyle = c();
+            ctx.fill();
             ctx.restore();
         }
         return null;
